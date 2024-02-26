8b5b83cf34b11c494ead728f2c0d0b67433b1e39
Injostis <jokerrod06@outlook.com>
2024-02-25 19:08:45 -0600

    Productos base

---
+++ resources/views/productos/create.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Edición del producto</h1>
+    </head>
+    <body>
+        <h1>Edición del producto</h1>
+    </body>
+</html>
---
+++ resources/views/productos/index.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Vista del producto</h1>
+    </head>
+    <body>
+        <h1>Vista del producto</h1>
+    </body>
+</html>
